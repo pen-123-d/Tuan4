53b550b0f772f36dbb985eb23054e66d2d2c85ef
Pen-123-d <ppenpp556@gmail.com>
2024-08-05 22:57:42 +0700

    Cau 1.3 code hinh anh ket qua debug

--- Tuan4/Project6/Project6/Source.cpp
+++ Tuan4/Project6/Project6/Source.cpp
@@ -2,7 +2,22 @@
 
 using namespace std;
 
-int Tong(int n)
+
+double Bai_Mot_Cham_3 (double n)
+{
+	if (n==1)
+	{
+		return 0.5;
+	}
+	else
+	{
+		return +Bai_Mot_Cham_3(n - 1)+ n / (n + 1) ;
+	}
+
+}
+
+
+int Tong(double n)
 {
 	if (n == 1)
 	{
@@ -17,10 +32,13 @@ int Tong(int n)
 
 int main()
 {
-	int n;
+	double n;
 	cout << "Vui long nhap n " << endl;
 	cin >> n;
 	cout << "De quy tong" << endl;
 	cout << Tong(n) << endl;
+	cout << "Bai 1.3 De quy phan so " << endl;
+	cout << Bai_Mot_Cham_3(n)<< endl;
+
 	system("pause");
 }
